e1e5d9a88bb2e46533e89fafcd9ef7ba86f898b6
loidpadre <loidpadre@gmail.com>
2025-01-16 20:20:37 -0300

    Conta do reataurante

---
+++ 02-contaDoRestaurante.js
@@ -0,0 +1,7 @@
+// exercicio 2 elaborar um programa que leia o valor de um jantar,
+//  calcule  e informe o valor da taxa do garçom (10%) e o valor total a ser pago
+let valor_jantar = 80;
+let taxa_garcon = (10 / 100) * valor_jantar;
+let total_pagar = valor_jantar + taxa_garcon; 
+console.log(`Sr. a taxa do garçom é de 10%, que pelo valor do jantar ${valor_jantar} R$, fica ${taxa_garcon} R$, sendo assim o valor total é: ${total_pagar} R$`)
+
