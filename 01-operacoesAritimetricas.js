f215f4215dec8d50b1f32ab7f94010c6f474ad67
loidpadre <loidpadre@gmail.com>
2025-01-16 20:02:34 -0300

    Primeiro

---
+++ 01-operacoesAritimetricas.js
@@ -0,0 +1,15 @@
+let soma = 0;
+let multiplicacao = 0;
+let sub = 0;
+let divisao = 0;
+let num1 = 2;
+let num2 = 4;
+
+soma = num1 + num2
+multiplicacao = num1 * num2
+sub = num1 - num2
+divisao = num1 / num2
+console.log(`a soma de ${num1} + ${num2} = ${soma}`)
+console.log(`a multiplicação de ${num1} * ${num2} = ${multiplicacao}`)
+console.log(`a subtração de ${num1} - ${num2} = ${sub}`)
+console.log(`a divsão de ${num1} / ${num2} = ${divisao}`)
